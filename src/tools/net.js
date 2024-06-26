dd2203d515224bb646460cfcdbd398321c4e6782
zhangruyi <ryzhang97@qq.com>
2024-06-26 18:47:17 +0800

    初始化:axios

---
+++ src/tools/net.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+const env = import.meta.env
+axios.defaults.timeout = env.VITE_API_TIMEOUT
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+// 添加请求拦截器
+axios.interceptors.request.use(config => {
+    console.log(config);
+    return config
+}, err => {
+    return Promise.reject(err)
+})
+
+
+// 添加响应拦截器
+axios.interceptors.response.use(res => {
+    console.log(res);
+    return res;
+}, err => {
+    return Promise.reject(err)
+})
+//获取url
+const getUrl = (url) => {
+    return "/api" + url
+}
+//获取日志
+const getConfig = (config={}) => {
+    config.headers = getHeaders(config.headers)
+    return config
+}
+//获取header
+const getHeaders = (headers = {}) => {
+    headers['Content-Type'] = 'application/json;charset=UTF-8'
+    return headers
+}
+
+//post
+export async function post(url, params={}, config={}) {
+    return axios.post(getUrl(url), params, getConfig(config))
+}
+
+//get
+export async function get(url, params={}, config={}) {
+    return axios.get(getUrl(url), {params}, getConfig(config))
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+
+const env = import.meta.env
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: ['@babel/plugin-transform-react-jsx'],
-    },
-  })],
-  server: {
-    host: "0.0.0.0"
-  }
+    plugins: [react({
+        babel: {
+            plugins: ['@babel/plugin-transform-react-jsx'],
+        },
+    })],
+    server: {
+        host: "0.0.0.0",
+        proxy: {
+            '/api': {
+                target: "http://127.0.0.1:8000/", // 目标接口域名
+            },
+        },
+    }
 })
