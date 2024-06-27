963404fd6cc11721599e3f2791aa9f050188cef6
zhangruyi <1067852@greatwall.com.cn>
2024-06-27 22:35:38 +0800

    在函数组件中使用ant design

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
+import {Input, Button} from 'antd';
 
-class Page extends React.Component {
-    state = {
-        message: 'login',
-    };
+const Page = () => {
+    return (
+        <div>
+            <Input placeholder="username"/>
+            <Input placeholder="password"/>
+            <Button type="primary" block>login</Button>
+        </div>
+    );
+};
 
-    //挂载前
-    constructor(props) {
-        super(props);
-        console.log("挂载前")
-    }
-
-    //挂载后
-    componentDidMount() {
-        console.log("挂载后")
-    }
-
-    // 是否继续渲染组件
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("是否继续渲染组件")
-        return true
-    }
-
-    // 组件重新渲染后
-    componentDidUpdate(prevProps, prevState) {
-        console.log("组件重新渲染后")
-    }
-
-    //组件即将移除
-    componentWillUnmount() {
-        console.log('组件即将移除');
-    }
-
-    render() {
-        return <div>{this.state.message}</div>
-    }
-
-}
-
-export default Page
+export default Page;
