496922939c779586297a04ebacbbb1d4fcb314b4
zhangruyi <1067852@greatwall.com.cn>
2024-06-29 19:24:11 +0800

    使用自定义hooks 使用生命周期hooks 获取查询参数和页面跳转

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSearchParams, useNavigate} from 'react-router-dom';
 import {Input, Button} from 'antd';
+import useData from '@/hooks/useData'
+
 
 const Page = () => {
+    const {data, dataUpdate} = useData({openid: ""});
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+    let openid = searchParams.get('openid');
+    useEffect(() => {
+        console.log(openid)
+        dataUpdate({openid})
+        return () => {
+            console.log('Component is unmounting');
+        };
+    }, []);
+
+    function onLogin() {
+        console.log("openid:", openid)
+        navigate('/home');
+    }
+
     return (
         <div>
-            <Input placeholder="username"/>
+            <Input placeholder="username" value={data.openid}/>
             <Input placeholder="password"/>
-            <Button type="primary" block>login</Button>
+            <Button type="primary" block onClick={onLogin}>login</Button>
         </div>
     );
 };
